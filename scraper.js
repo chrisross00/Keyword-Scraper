69f20236451c9f876eae6103ce3a3b429b221195
chris_macbook <chrisross00@gmail.com>
2018-02-17 17:13:37 -0700

    Updates 2/17
    Updated some styling for add url buttons;
    
    added a clear elementsForUI button;
    
    separated scraper to its own function and to call within for loop

--- scraper.js
+++ scraper.js
@@ -14,91 +14,110 @@ var request = require('../node_modules/request');
 var fs = require('fs');
 var cheerio = require('cheerio');
 var querystring = require('../node_modules/querystring');
+var elementsForUI = [];
 
 app = express();
 exports.getPageData = getPageData;
+exports.clearScraper = clearScraper;
+
+function clearScraper(req, res){
+    elementsForUI = [];
+    console.log("called clearScraper " + elementsForUI);
+}
 
 // Assign everything from AJAX call to a variable
-function getPageData(req,res){
-    console.log("got page element as " + req.query.pageElement);
-    var pageElement = req.query.pageElement.toString();
-    var childElement = req.query.childElement.toString();
-    var subChildElement = req.query.subChildElement.toString();
-    var reqLogin = req.query.reqLogin;
+    function getPageData(req,res){
+        console.log("got page element as " + req.query.pageElement);
 
-// Need to make url interpret JSON url return
-    url = decodeURIComponent(req.query.inputUrl);
-    url = JSON.stringify(url);
-    console.log("stringified url = " + url);
-
-    console.log("checked or not checked " + req.query.reqLogin);
-    
-// If scraping requires login first, go through an authentication workflow
-    // if(reqLogin == 'true'){
+    // Need to make url interpret JSON url return
+        var urls = [];
+        for(var i = 0; i<req.query.inputUrl.length; i++){
+            urls.push(decodeURIComponent(req.query.inputUrl[i]));
+        }
         
-    // }
-    
-    // _token:8m7wcbr7r4HR9AisRccBYWgS1G2fapSsXHxHD7CC
+    // If scraping requires login first, go through an authentication workflow
+        // console.log("checked or not checked " + req.query.reqLogin);
+        // if(reqLogin == 'true'){
+            
+        // }
+        
+        // _token:8m7wcbr7r4HR9AisRccBYWgS1G2fapSsXHxHD7CC
 
-// If scraping doesn't require a login, scrape the page passed through AJAX call
+    // If scraping doesn't require a login, scrape the page passed through AJAX call
 
-    request(url, function(error, response, body){
-        if(!error){
-            var $ = cheerio.load(body);
-            var elementCount;
-            var elementList = [];
+    // app.get(function(req,res){
+        for(var i = 0; i < urls.length; i++){
 
-        // Get the site name
-            var site = $('head').find('title');
-            var siteName = site.text();
+            scrapePage(urls[i], req, res);
+        }
+        console.log(elementsForUI);
+    };
 
-        // Grab each url and make a list of pageElements
 
-        // Grab each page element and parse the desired data
-            $(pageElement).each(function(index, element){
-                elementList[index] = {};
-                if(childElement != ''){
-                    var header = $(element).find(childElement);
-                    if(subChildElement != ''){
-                        elementList[index] = $(header).find(subChildElement).text();
+    function scrapePage(url, req, res){
+        request.get(url, function(error, response, body){
+            var pageElement = req.query.pageElement.toString();
+            var childElement = req.query.childElement.toString();
+            var subChildElement = req.query.subChildElement.toString();
+            var reqLogin = req.query.reqLogin;
+            if(!error){
+                console.log("Hit the if statement and using url = " + url);
+                var $ = cheerio.load(body);
+                var elementCount;
+                var elementList = [];
+
+            // Get the site name
+                var site = $('head').find('title');
+                var siteName = site.text();
+
+            // Grab each url and make a list of pageElements
+
+            // Grab each page element and parse the desired data
+                $(pageElement).each(function(index, element){
+                    elementList[index] = {};
+                    if(childElement != ''){
+                        var header = $(element).find(childElement);
+                        if(subChildElement != ''){
+                            elementList[index] = $(header).find(subChildElement).text();
+                        }
+                        else{
+                            elementList[index] = $(header).text();
+                        }
                     }
                     else{
                         elementList[index] = $(header).text();
                     }
-                }
-                else{
-                    elementList[index] = $(header).text();
-                }
+                    //console.log(elementList[index]);
+                });
+            }
+            else{
+                console.log("Hit the else statement due to Error:\n\n:" + error);
+            }
+
+        // Bad character handling goes here
+
+        // Save JSON
+            fs.writeFile('output.json', JSON.stringify(elementList, null, 4), function(err){
+                    // console.log('File successfully written! - Check your project directory for the output.json file');
             });
-        }
-        else{
-            console.log("Error:\n\n:" + error);
-        }
 
-    // Bad character handling goes here
-
-    // Save JSON
-        fs.writeFile('output.json', JSON.stringify(elementList, null, 4), function(err){
-                // console.log('File successfully written! - Check your project directory for the output.json file');
-        });
-
-    // Format for HTML display
-        elementList.forEach(function(el,i){
-            elementList[i] = "<li>" + elementList[i];
-        });
-
-    // Pass element data to other modules
-        module.exports.elementList = elementList;
-        module.exports.elementCount = elementCount;
-
-    // Send element data back to UI
-        res.send(
-            {'id': elementList,
-            'url': url,
-            'site': siteName
+        // Format for HTML display
+            elementList.forEach(function(el,i){
+                elementsForUI.push("<li>" + elementList[i]);
+                console.log(i + " i " + elementsForUI[i]);
             });
-    });
-};
+
+        // Pass element data to other modules
+            module.exports.elementList = elementsForUI;
+        
+        // Send element data back to UI
+            res.send(
+                {'id': elementsForUI,
+                'url': url,
+                'site': siteName
+                });  
+        });
+    }
 
 
 
