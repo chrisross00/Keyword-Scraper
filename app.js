88b7891ecc68ab7fabdf3275207bfe09ea1e5ac1
chris_macbook <chrisross00@gmail.com>
2018-04-08 17:18:07 -0600

    Removed hardcoded dropdown items
    - Added extDictionaryData function to scraper.js; function gets local file and saves data to a variable, and passes that variable as an ajax response
    - Put .get(extDictionaryData) call in app.js
    - Put ajax call to extDictionaryData in script tag to populate the dropdown on page load

--- app.js
+++ app.js
@@ -7,14 +7,16 @@
 
 var http = require('http');
 var scraper = require('./scraper');
+// var extDictionary = require('./siteDictionary.json');
 
-var express = require('../node_modules/express');
+var express = require('./node_modules/express');
 var app = express();
 app.use(express.static((__dirname, 'public')));
+
+app.get('/extDictionary', scraper.extDictionaryData);
 app.get('/scraper', scraper.getPageData);
 app.get('/clearScraper', scraper.clearScraper);
 
 app.listen(8888,function(){ // spin up an local server
 	console.log("App is started on port number 8888 and enjoy!")
 });
-
